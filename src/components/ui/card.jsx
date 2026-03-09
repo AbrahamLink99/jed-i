b02e88c2785e10cacccbec6390cbc3b62473164c
Linus Karlsson <linuskarlssonf12@gmail.com>
2026-03-09 08:48:46 +0000

    File changes

--- src/components/ui/card.jsx
+++ src/components/ui/card.jsx
@@ -1,4 +1,3 @@
-
 import * as React from "react"
 
 import { cn } from "@/lib/utils"
@@ -6,7 +5,7 @@ import { cn } from "@/lib/utils"
 const Card = React.forwardRef(({ className, ...props }, ref) => (
   <div
     ref={ref}
-    className={cn("rounded-2xl bg-white text-card-foreground shadow-[0_1px_3px_rgba(0,0,0,0.06)]", className) }
+    className={cn("rounded-2xl bg-white text-card-foreground shadow-[0_1px_2px_rgba(0,0,0,0.04)] ring-1 ring-black/[0.04]", className) }
     {...props} />
 ))
 Card.displayName = "Card"
